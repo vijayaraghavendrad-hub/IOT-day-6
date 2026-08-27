d7a1b6277a300905f303c5cdf18c8f3aaa25093e
vijayaraghavendrad-hub <vijayaraghavendrad@gmail.com>
2026-08-27 10:50:58 +0530

    First commit

---
+++ IOT6/src/IOT6/bak_trk_ex2.java
@@ -0,0 +1,23 @@
+package IOT6;
+
+public class bak_trk_ex2 {
+
+    public static void main(String[] args) {
+
+        String correctPassword = "231";
+        String[] passwords = {"123", "132", "213", "231"};
+
+        for (String password : passwords) {
+
+            System.out.println("Trying: " + password);
+
+            if (password.equals(correctPassword)) {
+                System.out.println("Password found: " + password);
+                break;
+            }
+            else {
+            System.out.println("Wrong password. Try another.");
+        }
+    }
+}
+}
